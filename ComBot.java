6d4bec123f8bea0beb6c94e3869f1658ffe4170b
DWiz24 <bobthequark@gmail.com>
2018-01-10 16:00:51 -0500

    Now actually builds rangers

--- ComBot.java
+++ ComBot.java
@@ -1,8 +1,14 @@
 // import the API.
 // See xxx for the javadocs.
 import bc.*;
-
 class ComBot {
-static Direction[] dirs = Direction.values();
+   static Direction[] dirs = Direction.values();
+   static GameController gc;
+   static int fights=8;
+   static  Location[] fights=new Location[fights];
+   static void turn(GameController gameC) {
+      gc=gameC;
+      
+   }
 
 }
