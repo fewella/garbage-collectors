ad49dbcefdbdcb73f321766d80db3526bbdb2c0e
etherlyt <hcx2001@gmail.com>
2018-01-14 12:44:56 -0500

    Sort respective unit types

--- Player.java
+++ Player.java
@@ -1,7 +1,8 @@
 // import the API.
 // See xxx for the javadocs.
 import bc.*;
-
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.PriorityQueue;
 
 public class Player {
@@ -19,6 +20,31 @@ public class Player {
             mapEarth = gc.startingMap(Planet.Earth);
             mapMars = gc.startingMap(Planet.Mars);
 
+            Queue<Unit> worker = new LinkedList<Unit>();
+            Queue<Unit> knight = new LinkedList<Unit>();
+            Queue<Unit> ranger = new LinkedList<Unit>();
+            Queue<Unit> healer = new LinkedList<Unit>();
+            Queue<Unit> mage = new LinkedList<Unit>();
+            Queue<Unit> factory = new LinkedList<Unit>();
+            Queue<Unit> rocket = new LinkedList<Unit>();
+            VecUnit units = gc.myUnits();
+            for(int i = 0; i < units.size(); i++) {
+            	Unit temp = units.get(i);
+            	if (temp.unitType()==UnitType.Worker) 
+            		worker.add(temp);
+            	else if (temp.unitType()==UnitType.Knight) 
+            		knight.add(temp);
+            	else if (temp.unitType()==UnitType.Ranger) 
+            		ranger.add(temp);
+            	else if (temp.unitType()==UnitType.Healer) 
+            		healer.add(temp);
+            	else if (temp.unitType()==UnitType.Mage) 
+            		mage.add(temp);
+            	else if (temp.unitType()==UnitType.Factory) 
+            		factory.add(temp);
+            	else
+            		rocket.add(temp);
+            }
             //map analysis
             MapAnalysis.setup();
 
