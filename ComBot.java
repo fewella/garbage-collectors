39c05eec7f0b227e7c001d103953f9be03584830
DWiz24 <bobthequark@gmail.com>
2018-01-13 19:37:39 -0500

    Gitignore now works with eclipse

--- ComBot.java
+++ ComBot.java
@@ -1,14 +1,18 @@
+import bc.*;
 // import the API.
 // See xxx for the javadocs.
-import bc.*;
+
+
+
 class ComBot {
    static Direction[] dirs = Direction.values();
    static GameController gc;
    static int fights=8;
    static  Location[] fight=new Location[fights];
+   static Unit[] myR=new Unit[200];
+   static int rangers=0;
    static void turn(GameController gameC) {
       gc=gameC;
-      
+      VecUnit us=gc.units();
    }
-
 }
