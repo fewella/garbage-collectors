0bf93df3e8d7168bf634fc06f9e7df86aa00b08a
BrokenToaster1 <aleksey.didenkov@gmail.com>
2018-01-15 01:11:02 -0500

    Resolving merge conflicts

--- Player.java
+++ Player.java
@@ -1,8 +1,7 @@
 // import the API.
 // See xxx for the javadocs.
 import bc.*;
-
-import java.util.PriorityQueue;
+import java.util.*;
 
 public class Player {
     static Direction[] dirs  = Direction.values();
@@ -10,8 +9,16 @@ public class Player {
     //global variables
     static GameController gc;
     static PlanetMap map, mapEarth, mapMars;
+    static Veci32 arrayEarth, arrayMars;
 
     public static void main(String[] args) {
+        Queue<Unit> worker = new LinkedList<Unit>();
+        Queue<Unit> knight = new LinkedList<Unit>();
+        Queue<Unit> ranger = new LinkedList<Unit>();
+        Queue<Unit> healer = new LinkedList<Unit>();
+        Queue<Unit> mage = new LinkedList<Unit>();
+        Queue<Unit> factory = new LinkedList<Unit>();
+        Queue<Unit> rocket = new LinkedList<Unit>();
         try {
             //connect to the manager, starting the game
             gc = new GameController();
@@ -36,7 +43,32 @@ public class Player {
         while (true) {
             try{
                 //game cycle
-                Econ.turn(gc);
+                arrayEarth = gc.getTeamArray(Planet.Earth);
+                arrayMars = gc.getTeamArray(Planet.Mars);
+
+                VecUnit units = gc.myUnits();
+                //System.out.println("Units "+ units.size());
+                for(int i = 0; i < units.size(); i++) {
+                    Unit temp = units.get(i);
+                    if (temp.unitType()==UnitType.Worker) {
+                        worker.add(temp);
+                        //System.out.println("added worker");
+                    }
+                    else if (temp.unitType()==UnitType.Knight)
+                        knight.add(temp);
+                    else if (temp.unitType()==UnitType.Ranger)
+                        ranger.add(temp);
+                    else if (temp.unitType()==UnitType.Healer)
+                        healer.add(temp);
+                    else if (temp.unitType()==UnitType.Mage)
+                        mage.add(temp);
+                    else if (temp.unitType()==UnitType.Factory)
+                        factory.add(temp);
+                    else
+                        rocket.add(temp);
+                }
+                MapAnalysis.turn();
+                Econ.turn(gc, worker, factory);
                 ComBot.turn(gc);
                 gc.nextTurn();
             }
