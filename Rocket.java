3ccacf6882946f96c23d373975c705fd205d492d
BrokenToaster1 <aleksey.didenkov@gmail.com>
2018-01-17 19:17:55 -0500

    Rocket Landing

--- Rocket.java
+++ Rocket.java
@@ -2,10 +2,8 @@ import bc.*;
 import java.util.*;
 class Rocket {
 	static Direction[] dirs = Direction.values();
+	static int[][] occupied;
 	public static void launch(GameController gc) {
-		long x = Player.mapMars.getWidth()/2;
-		long y = x;
-		MapLocation dest = new MapLocation(Planet.Mars, Math.toIntExact(x), Math.toIntExact(y));
 		for( Unit r:Player.rocket) {
 			if(r.structureGarrison().size() != r.structureMaxCapacity() && r.location().isOnPlanet(Planet.Earth)) {
 				VecUnit loUnit = gc.senseNearbyUnitsByTeam(r.location().mapLocation(),1 , gc.team());
@@ -15,8 +13,30 @@ class Rocket {
 				}
 			}
 			if(r.structureGarrison().size() == r.structureMaxCapacity() || r.health() < 120 || gc.round() > 500) {
+				short[][] karb = MapAnalysis.karboniteMatMars((int)(gc.round()+gc.currentDurationOfFlight())).x;
+				int minScore = 9999;
+				MapLocation dest = new MapLocation(Planet.Mars, 10, 10);	//shouldn't happen
+				for(int y = 0; y < Player.mapMars.getHeight(); y++){
+					for(int x = 0; x < Player.mapMars.getWidth(); x++) {
+						if (MapAnalysis.passabilityMatMars[y][x] == 0) continue;
+						if (occupied[y][x] == 1) continue;
+						int score = 5 * karb[y][x] - MapAnalysis.karboniteConnected(Planet.Mars, x, y);
+						for (int dy = -1; dy <= 1; dy++) {
+							if (y + dy < 0 || y + dy >= Player.mapMars.getHeight()) continue;
+							for (int dx = -1; dx <= 1; dx++) {
+								if (x + dx < 0 || x + dx >= Player.mapMars.getWidth()) continue;
+								score -= karb[y+dy][x+dx];
+							}
+						}
+						if(score < minScore){
+							minScore = score;
+							dest = new MapLocation(Planet.Mars, x, y);
+						}
+					}
+				}
 				if( gc.canLaunchRocket(r.id(), dest) ) {
 					gc.launchRocket(r.id(), dest);
+					occupied[dest.getY()][dest.getX()] = 1;
 					System.out.println("Launched");
 				}
 				else
