e4dcc02c870c9b7fc0a547b27b67a213ba0590d2
DWiz24 <bobthequark@gmail.com>
2018-01-23 18:28:43 -0500

    should fix Killed

--- Player.java
+++ Player.java
@@ -45,9 +45,7 @@ public class Player {
         while (true) {
             try {
 	            //game cycle
-	            if (gc.round() % 2 == 0) {
-		            System.gc();
-	            }
+		        System.gc();
 	            worker = new LinkedList<>();
 	            knight = new LinkedList<>();
 	            ranger = new LinkedList<>();
